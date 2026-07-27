ef341faba30302a9ee041c1ea3c5296453b46570
ryantang <ryan.tang@quizlet.com>
2026-07-26 19:26:21 -0700

    refactor log_levels.cpp

--- log-levels/log_levels.cpp
+++ log-levels/log_levels.cpp
@@ -1,22 +1,21 @@
 #include <string>
-#include <iostream>
 
 namespace log_line {
-std::string message(std::string line) {
-    int colonIndex = line.find(':');
-    int messageStart = colonIndex + 2;
-    return line.substr(messageStart);
+std::string message(const std::string& line) {
+    size_t colon_index = line.find(':');
+    size_t message_start = colon_index + 2; // skips ": " in the log line
+    return line.substr(message_start);
 }
 
-std::string log_level(std::string line) {
-    int leftBracketIndex = line.find('[');
-    int rightBracketIndex = line.find(']');
-    int levelLeftIndex = leftBracketIndex + 1;
-    int levelLength = rightBracketIndex - leftBracketIndex - 1;
-    return line.substr(levelLeftIndex, levelLength);
+std::string log_level(const std::string& line) {
+    size_t left_bracket_index = line.find('[');
+    size_t right_bracket_index = line.find(']');
+    size_t level_left_index = left_bracket_index + 1;
+    size_t level_length = right_bracket_index - left_bracket_index - 1;
+    return line.substr(level_left_index, level_length);
 }
 
-std::string reformat(std::string line) {
+std::string reformat(const std::string& line) {
     return message(line) + " (" + log_level(line) + ")";
 }
 }  // namespace log_line
