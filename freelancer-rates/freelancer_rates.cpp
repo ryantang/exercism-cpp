d98fa9aad0f0d6af1e1c9d0f6c173db9e978f5f1
ryantang <ryan.tang@quizlet.com>
2026-07-27 12:26:07 -0700

    complete freelancer_rates exercise

---
+++ freelancer-rates/freelancer_rates.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+// INFO: Headers from the standard library should be inserted at the top via
+// #include <LIBRARY_NAME>
+
+// daily_rate calculates the daily rate given an hourly rate
+double HOURS_IN_WORKDAY{8.0};
+double daily_rate(double hourly_rate) {
+    return hourly_rate * HOURS_IN_WORKDAY;
+}
+
+// apply_discount calculates the price after a discount
+double apply_discount(double before_discount, double discount) {
+    return before_discount * (100.0 - discount) / 100.00; // convert discount to a fraction
+}
+
+// monthly_rate calculates the monthly rate, given an hourly rate and a discount
+// The returned monthly rate is rounded up to the nearest integer.
+double DAYS_IN_MONTH{22.0};
+int monthly_rate(double hourly_rate, double discount) {
+    double discounted_monthly_rate{apply_discount(daily_rate(hourly_rate) * DAYS_IN_MONTH, discount)};
+    return static_cast<int>(std::ceil(discounted_monthly_rate));
+}
+
+// days_in_budget calculates the number of workdays given a budget, hourly rate,
+// and discount The returned number of days is rounded down (take the floor) to
+// the next integer.
+int days_in_budget(int budget, double hourly_rate, double discount) {
+    double unrounded_days_in_budget{budget / apply_discount(daily_rate(hourly_rate), discount)};
+    return static_cast<int>(std::floor(unrounded_days_in_budget));
+}
