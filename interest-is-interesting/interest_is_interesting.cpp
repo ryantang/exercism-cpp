3deccc5c5e4a5e229ea8568755c4ea168722df0f
ryantang <ryan.tang@quizlet.com>
2026-08-05 11:19:13 -0700

    refactor interest_is_interesting

--- interest-is-interesting/interest_is_interesting.cpp
+++ interest-is-interesting/interest_is_interesting.cpp
@@ -18,26 +18,21 @@ double interest_rate(double balance) {
 
 // yearly_interest calculates the yearly interest for the provided balance.
 double yearly_interest(double balance) {
-    // TODO: Implement the yearly_interest function
     return balance * interest_rate(balance) / 100.0;
 }
 
 // annual_balance_update calculates the annual balance update, taking into
 // account the interest rate.
 double annual_balance_update(double balance) {
-    // TODO: Implement the annual_balance_update function
     return balance + yearly_interest(balance);
 }
 
 // years_until_desired_balance calculates the minimum number of years required
 // to reach the desired balance.
 int years_until_desired_balance(double balance, double target_balance) {
-    if (balance >= target_balance) {
-        return 0;
-    }
+    int years{0};
+    double updated_balance{balance};
 
-    int years{1};
-    double updated_balance{annual_balance_update(balance)};
     while (updated_balance < target_balance) {
         updated_balance = annual_balance_update(updated_balance);
         years++;
